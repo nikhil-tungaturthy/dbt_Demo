5e13a569fc5180da5d3ef1b8a250067c659c267d
Nikhil Tungaturthy <nikhil.tungaturthy@fishtownanalytics.com>
2025-10-14 19:32:05 +0000

    test

--- models/orders_min.sql
+++ models/orders_min.sql
@@ -1,5 +1,7 @@
-{{ config(materialized='table') }}
+{{ config(materialized='view') }}
 
-select order_key, order_date
-from {{ source('raw','fact_order') }}
-
+select 
+    order_key, 
+    year(order_date) as order_year
+from 
+    {{ source('raw','fact_order') }}
